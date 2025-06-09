9e4bb237a8fbb693c10d4a6e0bc645b10251608a
mariricken <146238773+mariricken@users.noreply.github.com>
2025-06-09 08:19:41 -0300

    .

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 document.querySelector('.parametro-senha_botão');
 const numeroSenha = document.querySelector('.parametro-senha_texto');
 numeroSenha.textContent = 5;
+
+const botoes = document.querySelector('.parametro-senha_botao');
+
+console.log(botoes);
